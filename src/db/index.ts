e335053c0f5f38887da26535a6e7393e86fcd329
aluizio-jr <aluizio.fg@gmail.com>
2022-01-27 10:31:15 -0300

    global connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,11 @@
 export async function connect(){
-    if(global.connection && global.connection.state !== 'disconnected')
-        return global.connection;
+    let globalWithConnection = global as typeof globalThis & {connection: any}
+    if(globalWithConnection.connection && globalWithConnection.connection.state !== 'disconnected')
+        return globalWithConnection.connection;
  
     const mysql = require("mysql2/promise");
     const connection = await mysql.createConnection("mysql://fitgroup_erp:dhvtnc0809vps@fitgroup.com.br:3306/fitgroup_erp");
     
-    global.connection = connection;
+    globalWithConnection.connection = connection;
     return connection;
 }
